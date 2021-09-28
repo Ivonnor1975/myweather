b413a5719097678faa8bac1e2801c028bb4bf60b
Ivon <ivonne@jjf.com>
2021-09-27 20:57:06 -0500

    add city search and lat & lon search

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,72 +1,84 @@
+var dia=moment().format('l')
 var userFormEl = document.querySelector("#user-form");
-var languageButtonsEl = document.querySelector("#language-buttons");
-var nameInputEl = document.querySelector("#username");
+var cityButtonsEl = document.querySelector("#city-buttons");
+var nameInputEl = document.querySelector("#cityname");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
 
   // get value from input element
-  var username = nameInputEl.value.trim();
+  var cityname = nameInputEl.value.trim();
 
-  if (username) {
-    getUserRepos(username);
+  if (cityname) {
+    getcity(cityname);
 
     // clear old content
     repoContainerEl.textContent = "";
     nameInputEl.value = "";
   } else {
-    alert("Please enter a GitHub username");
+    alert("Please enter a city");
   }
 };
 
-var buttonClickHandler = function(event) {
-  // get the language attribute from the clicked element
-  var language = event.target.getAttribute("data-language");
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    // clear old content
-    repoContainerEl.textContent = "";
-  }
-};
-
-var getUserRepos = function(user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
+var getcity = function(city) {
+  // format the weather api url by city
+  var apiUrl=  "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&APPID=126a080714e51ffd3ce7f4eabeb126fc";
   // make a get request to url
   fetch(apiUrl)
     .then(function(response) {
       // request was successful
       if (response.ok) {
-        console.log(response);
-        response.json().then(function(data) {
-          console.log(data);
-          displayRepos(data, user);
+          response.json().then(function(data) {
+          //create buttons and save locations
+          getmoredetails(data.coord.lat, data.coord.lon, data.name);
         });
       } else {
         alert("Error: " + response.statusText);
       }
     })
     .catch(function(error) {
-      alert("Unable to connect to GitHub");
+      alert("Unable to connect to Weather Web site");
     });
 };
 
-var getFeaturedRepos = function(language) {
+var getmoredetails = function(citylat, citylon,cityname) {
   // format the github api url
-  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-
+  var apiUrl= "https://api.openweathermap.org/data/2.5/onecall?lat="+citylat+"&lon="+citylon+"&units=imperial&appid=126a080714e51ffd3ce7f4eabeb126fc";
   // make a get request to url
   fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
       response.json().then(function(data) {
-        displayRepos(data.items, language);
+        console.log(data);
+        repoSearchTerm.textContent = cityname + " ( "+dia+")";
+        temp.textContent = data.current.temp+"F";
+        wind.textContent = data.current.wind_speed+"MPH";
+        humity.textContent = data.current.humidity+"%";
+        uvindex.textContent= data.current.uvi;
+        //date0.textContent= 
+        temp0.textContent = data.daily[0].temp.day+"F";
+        wind0.textContent = data.daily[0].wind_speed+"MPH";
+        humity0.textContent = data.daily[0].humidity+"%";
+        //date1.textContent= 
+        temp0.textContent = data.daily[1].temp.day+"F";
+        wind0.textContent = data.daily[1].wind_speed+"MPH";
+        humity0.textContent = data.daily[1].humidity+"%";
+        //date2.textContent= 
+        temp0.textContent = data.daily[2].temp.day+"F";
+        wind0.textContent = data.daily[2].wind_speed+"MPH";
+        humity0.textContent = data.daily[2].humidity+"%";
+        //date3.textContent= 
+        temp0.textContent = data.daily[3].temp.day+"F";
+        wind0.textContent = data.daily[3].wind_speed+"MPH";
+        humity0.textContent = data.daily[3].humidity+"%";
+        //date4.textContent= 
+        temp0.textContent = data.daily[4].temp.day+"F";
+        wind0.textContent = data.daily[4].wind_speed+"MPH";
+        humity0.textContent = data.daily[4].humidity+"%";
       });
     } else {
       alert("Error: " + response.statusText);
@@ -74,52 +86,18 @@ var getFeaturedRepos = function(language) {
   });
 };
 
-var displayRepos = function(repos, searchTerm) {
-  // check if api returned any repos
-  if (repos.length === 0) {
-    repoContainerEl.textContent = "No repositories found.";
-    return;
-  }
 
-  repoSearchTerm.textContent = searchTerm;
+var buttonClickHandler = function(event) {
+  // get the city attribute from the clicked element
+  var city = event.target.getAttribute("data-city");
 
-  // loop over repos
-  for (var i = 0; i < repos.length; i++) {
-    // format repo name
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
+  if (city) {
+    getFeaturedRepos(city);
 
-    // create a link for each repo
-    var repoEl = document.createElement("a");
-    repoEl.classList = "list-item flex-row justify-space-between align-center";
-    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
-
-    // create a span element to hold repository name
-    var titleEl = document.createElement("span");
-    titleEl.textContent = repoName;
-
-    // append to container
-    repoEl.appendChild(titleEl);
-
-    // create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
-
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    // append to container
-    repoEl.appendChild(statusEl);
-
-    // append container to the dom
-    repoContainerEl.appendChild(repoEl);
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
-
 // add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
-languageButtonsEl.addEventListener("click", buttonClickHandler);
+cityButtonsEl.addEventListener("click", buttonClickHandler);
